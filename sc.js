5761563ed2872bffb12585f14aa5deba1715f33a
Mat0123456789 <655021000337@mail.rmutk.ac.th>
2024-09-24 13:34:52 +0700

    Initial commit

---
+++ sc.js
@@ -0,0 +1,8 @@
+function changTitle(){
+    document.getElementById('title').innerHTML='Google Page';
+}
+function cal() {
+    let r = parseFloat(document.getElementById('txt').value)
+   /* document.getElementById('result').innerHTML = document.getElementById('txt').value; */
+   document.getElementById('result').innerHTML = 3.1459 * r * r;
+}
